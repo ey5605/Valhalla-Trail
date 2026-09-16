dda60a6a52c7f434e04bead205f534077b205fbe
ey5605 <eran.yehuda@gmail.com>
2026-09-16 20:29:24 +0300

    Fix stale service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='valhalla-trail-v1';
+const CACHE='valhalla-trail-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
